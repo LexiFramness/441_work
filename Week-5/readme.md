019ca8a47aa622e1b313aba03cac08d0bb4ba95e
lexiframness <lexiframness@gmail.com>
2019-02-16 19:11:37 -0600

    Week 5

---
+++ Week-5/readme.md
@@ -0,0 +1,9 @@
+## Week 5
+
+This weeks assignment was to create a memory game.
+
+# Workflow
+I decided to create a memory game using images from Marvel. Coding wise it was fairly straight forward. I knew I wanted to create this game using a table, which meant I needed to learn about <table> in HTML, which I hadn't really coded with much before.
+
+# Problems
+The biggest issue I had this week was accidentally leaving the closing bracket off a function in my main.js file. Which resulted in my images not loading. I am a little embarrassed at how long it took me to realize what the problem was. I definitely learned to check for errors using the "Inspect Element" in Firefox. It helped point me in the right direction and the problem would of been fixed much faster. I was convince I just had the file path incorrect leading to the images, and wasted a lot of time, staring at code that didn't have any problems.
---
+++ Week-5/script/main.js
@@ -0,0 +1,36 @@
+
+var imgTag = ["img1", "img2", "img3", "img4", "img5", "img6", "img7", "img8", "img9", "img10", "img11", "img12", "img13", "img14", "img15", "img16", "img17", "img18", "img19", "img20" ];
+
+var imgBlankGaming = "./images/marvel.jpg";
+
+var imgActual = new Array();
+
+
+function Gaming(){
+	randomImg();
+    for(var i = 0; i < imgTag.length; i++){
+        document.getElementById(imgTag[i]).src= imgBlankGaming;
+    }
+}
+
+function randomImg(){
+
+    var imgLink = ['./images/Spider.jpg', './images/Ironman.jpg', './images/Baddeadpool.jpg', './images/Hulk.jpg',  "./images/Captain.jpg",  "./images/Thor.jpg", "./images/Deadpool.jpg", "./images/Avengers.jpg", "./images/Widow.jpg", "./images/Shield.jpg"];
+
+    var count = [0,0,0,0,0,0,0,0,0,0];
+
+    while(imgActual.length < 20){
+
+        var randomNumber = Math.floor(Math.random() * imgLink.length)
+
+        if(count[randomNumber] < 2){
+            imgActual.push(imgLink[randomNumber]);
+            count[randomNumber] = count[randomNumber] + 1;
+        }
+    }
+}
+
+function flipImg(number){
+
+document.getElementById(imgTag[number]).src= imgActual[number];
+}
