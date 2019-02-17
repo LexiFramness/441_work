439bc384e8fb0d928fff268824f03b7816817b51
lexiframness <lexiframness@gmail.com>
2019-02-16 19:22:49 -0600

    Update main.js

--- Week-5/script/main.js
+++ Week-5/script/main.js
@@ -1,7 +1,7 @@
 
 var imgTag = ["img1", "img2", "img3", "img4", "img5", "img6", "img7", "img8", "img9", "img10", "img11", "img12", "img13", "img14", "img15", "img16", "img17", "img18", "img19", "img20" ];
 
-var imgBlankGaming = "./images/marvel.jpg";
+var imgBlankGaming = "./images/Marvel.jpg";
 
 var imgActual = new Array();
 
